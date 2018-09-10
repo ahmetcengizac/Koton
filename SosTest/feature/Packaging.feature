fd91b703ffca964f76ee99c5befbe7981564f51b
semracengiz <semracengiz@192.168.0.17>
2018-09-10 19:16:12 +0100

    Before packaging click

--- SosTest/feature/Packaging.feature
+++ SosTest/feature/Packaging.feature
@@ -2,11 +2,11 @@ Feature: Testing Packaging Process on SosTest Website
 
 Scenario: Part Weight Entry
 	Given I login sostestKoton website with correct supplier user 
-    | efeg | test1235 |
+    | efeg | test1234 |
 	When I click OrderManagement module in the OrderManagement menu
-	# And I navigate OrderManagement page
-	# And I select a job order 
-	# And I click Start Packaging button
+	  And I navigate OrderManagement page
+	  And I select a job order 
+	  And I click Start Packaging button
 	# And I click Part Weight Entry button
 	# And I navigate Order Weight Info page
 	# And I enter weight unit type gram 
--- SosTest/src/test/java/sosTest/steps/Packaging.java
+++ SosTest/src/test/java/sosTest/steps/Packaging.java
@@ -3,6 +3,8 @@ package sosTest.steps;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 
 import cucumber.api.DataTable;
 import cucumber.api.java.en.Given;
@@ -12,7 +14,6 @@ import sosTest.run.DriverFactory;
 
 public class Packaging extends DriverFactory {
 
-	
 		@Given("^I login sostestKoton website with correct supplier user$")
 		public void i_login_sostestKoton_website_with_correct_supplier_user(DataTable dataTable) throws Throwable {
 			
@@ -29,24 +30,53 @@ public class Packaging extends DriverFactory {
 
 		@When("^I click OrderManagement module in the OrderManagement menu$")
 		public void i_click_OrderManagement_module_in_the_OrderManagement_menu() throws Throwable {
-			driver.findElement(By.id("hel1"));
-			driver.findElement(By.id("el0")).click();
+
+			WebElement frme = driver.findElement(By.xpath("//frame[@name='header']"));
+
+			driver.switchTo().frame(frme);
+			Actions act =new Actions(driver);
+
+			act.moveToElement(driver.findElement(By.xpath("//div[@id='el1']"))).perform();
+
+			driver.switchTo().defaultContent();
+			
+			WebElement frmM = driver.findElement(By.xpath("//frame[@name='main']"));
+			driver.switchTo().frame(frmM);
+
+			WebElement myElement = driver.findElement(By.xpath("//div[@id='menu1']"));		
+			myElement.findElement(By.id("lnk0")).click(); //Order Management
 		}
 
 		@When("^I navigate OrderManagement page$")
 		public void i_navigate_OrderManagement_page() throws Throwable {
+			driver.switchTo().defaultContent();
+			WebElement frmM = driver.findElement(By.xpath("//frame[@name='main']"));
+			driver.switchTo().frame(frmM);
 			
+			WebElement elTable = driver.findElement(By.xpath("//table[@id='table']"));			
+			elTable.findElement(By.cssSelector("#detailRow_788 > td:nth-child(2)")).click();
+			
+			//System.out.println( elTable.getAttribute("innerHTML"));
+			
+			
+			//myElement.findElement(By.id("lnk0")).click(); //Order Management			
 		}
 
 		@When("^I select a job order$")
 		public void i_select_a_job_order() throws Throwable {
-			driver.findElement(By.id("788")).click();
+			WebElement elTable = driver.findElement(By.xpath("//table[@id='table']"));	
+			elTable.findElement(By.xpath("//input[@id='788']")).click();
 		}
 
 		@When("^I click Start Packaging button$")
 		public void i_click_Start_Packaging_button() throws Throwable {
-			driver.findElement(By.className("button-grp-wrp")).click();
+			WebElement elButtondiv = driver.findElement(By.className("button-grp-wrp"));	
+			System.out.println( elButtondiv.getAttribute("innerHTML"));
+			
+			
+			//elButtondiv.findElement(By.className("button button-vedi success")).click();	
 		}
+		
 
 		@When("^I click Part Weight Entry button$")
 		public void i_click_Part_Weight_Entry_button() throws Throwable {
